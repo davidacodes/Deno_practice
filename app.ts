b8cde1c294b8ffcf9db06033760dd861c8284ce4
davidalex620 <spand3r@gmail.com>
2020-05-14 07:18:08 -0500

    Added comment with link to tutorial

--- app.ts
+++ app.ts
@@ -1,129 +1,127 @@
-import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
+// https://www.freecodecamp.org/news/the-deno-handbook/#will-it-replace-node-js
 
-const env = Deno.env.toObject()
-const PORT = env.PORT || 4000
-const HOST = env.HOST || '127.0.0.1'
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const env = Deno.env.toObject();
+const PORT = env.PORT || 4000;
+const HOST = env.HOST || "127.0.0.1";
 
 interface Dog {
-    name: string
-    age: number
+  name: string;
+  age: number;
 }
 
 let dogs: Array<Dog> = [
-    {
-        name: 'Roger',
-        age: 8,
-    },
-    {
-        name: 'Syd',
-        age: 7,
-    },
-]
+  {
+    name: "Roger",
+    age: 8,
+  },
+  {
+    name: "Syd",
+    age: 7,
+  },
+];
 
 export const getDogs = ({ response }: { response: any }) => {
-    response.body = dogs
-}
+  response.body = dogs;
+};
 
 export const getDog = ({
-    params,
-    response,
+  params,
+  response,
 }: {
-    params: {
-        name: string
-    }
-    response: any
+  params: {
+    name: string;
+  };
+  response: any;
 }) => {
-    const dog = dogs.filter((dog) => dog.name === URLSearchParams.name)
-    if (dog.length) {
-        response.status = 200
-        response.body = dog[0]
-        return
-    }
+  const dog = dogs.filter((dog) => dog.name === URLSearchParams.name);
+  if (dog.length) {
+    response.status = 200;
+    response.body = dog[0];
+    return;
+  }
 
-    response.status = 400
-    response.body = { msg: `Cannot find dog ${params.name}`}
-}
+  response.status = 400;
+  response.body = { msg: `Cannot find dog ${params.name}` };
+};
 
 export const addDog = async ({
-    request,
-    response,
+  request,
+  response,
 }: {
-    request: any
-    response: any
+  request: any;
+  response: any;
 }) => {
-    const body = await request.body()
-    const dog: Dog = body.value
-    dogs.push(dog)
+  const body = await request.body();
+  const dog: Dog = body.value;
+  dogs.push(dog);
 
-    response.body = { msg: 'OK' }
-    response.status = 200
-}
+  response.body = { msg: "OK" };
+  response.status = 200;
+};
 
 export const updateDog = async ({
-    params,
-    request,
-    response,
+  params,
+  request,
+  response,
 }: {
-    params: {
-        name: string
-    }
-    request: any
-    response: any
+  params: {
+    name: string;
+  };
+  request: any;
+  response: any;
 }) => {
-    const temp = dogs.filter((existingDog) => existingDog.name === params.name)
-    const body = await request.body()
-    const { age }: { age: number } = body.value
+  const temp = dogs.filter((existingDog) => existingDog.name === params.name);
+  const body = await request.body();
+  const { age }: { age: number } = body.value;
 
-    if (temp.length) {
-        temp[0].age = age
-        response.status =  200
-        response.body = { msg: 'OK'}
-        return
-    }
+  if (temp.length) {
+    temp[0].age = age;
+    response.status = 200;
+    response.body = { msg: "OK" };
+    return;
+  }
 
-    response.status = 400
-    response.body = { msg: `Cannot find dog ${params.name}`}
-}
+  response.status = 400;
+  response.body = { msg: `Cannot find dog ${params.name}` };
+};
 
 export const removeDog = ({
-    params,
-    response,
+  params,
+  response,
 }: {
-    params: {
-        name: string
-    }
-    response: any
+  params: {
+    name: string;
+  };
+  response: any;
 }) => {
-    const lengthBefore = dogs.length
-    dogs = dogs.filter((dog) => dog.name !== params.name)
+  const lengthBefore = dogs.length;
+  dogs = dogs.filter((dog) => dog.name !== params.name);
 
-    if (dogs.length === lengthBefore) {
-        response.status = 400
-        response.body = { msg: `Cannot find dog ${params.name}`}
-        return
-    }
+  if (dogs.length === lengthBefore) {
+    response.status = 400;
+    response.body = { msg: `Cannot find dog ${params.name}` };
+    return;
+  }
 
-    response.body = { msg: 'OK' }
-    response.status = 200
-}
+  response.body = { msg: "OK" };
+  response.status = 200;
+};
 
-
-const router = new Router()
+const router = new Router();
 router
-    .get('/dogs', getDogs)
-    .get('/dogs/:name', getDog)
-    .post('/dogs', addDog)
-    .put('/dogs/:name', updateDog)
-    .delete('/dogs/:name', removeDog)
+  .get("/dogs", getDogs)
+  .get("/dogs/:name", getDog)
+  .post("/dogs", addDog)
+  .put("/dogs/:name", updateDog)
+  .delete("/dogs/:name", removeDog);
 
+const app = new Application();
 
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-const app = new Application
+console.log(`Listening on port ${PORT}...`);
 
-app.use(router.routes())
-app.use(router.allowedMethods())
-
-console.log(`Listening on port ${PORT}...`)
-
-await app.listen(`${HOST}:${PORT}`)
-
+await app.listen(`${HOST}:${PORT}`);
